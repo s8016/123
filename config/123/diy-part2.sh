41da8c0768defa8f6ffb9ea8aa9cba629a1a6dfd
s8016 <164966593+s8016@users.noreply.github.com>
2024-04-30 09:44:06 +0800

    Update diy-part2.sh

--- config/123/diy-part2.sh
+++ config/123/diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改 主机名称
-sed -i 's/ImmortalWrt/RAX-3000M/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 
 # 修改 闪存为256M
 sed -i 's/<0x580000 0x7200000>/<0x580000 0xea80000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
