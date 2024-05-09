f0ae6d55216dc874f04d6c35c769ec31fcaad60f
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-09 17:40:05 +0800

    Update diy-part1.sh

--- config/123/diy-part1.sh
+++ config/123/diy-part1.sh
@@ -10,18 +10,18 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # 添加第三方插件库
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+# echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
 
 
 # 添加额外软件包
 
 # 定时设置
-git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+# git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 
 # 魔改版-管控过滤[集成上网时间控制，黑白名单IP过滤，网址过滤几大功能]
-git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
+# git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
 
 # 管控过滤-普通版
 # git clone https://github.com/ywt114/luci-app-control-weburl package/luci-app-control-weburl
@@ -37,13 +37,13 @@ git clone https://github.com/selfcan/luci-app-onliner package/luci-app-onliner
 # git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 
 # 家长控制
-git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
+# git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 
 # 网络唤醒++
-git clone https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
+# git clone https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
 
 # 实时流量监控
-git clone https://github.com/firker/luci-app-wrtbwmon-zh package/luci-app-wrtbwmon-zh
+# git clone https://github.com/firker/luci-app-wrtbwmon-zh package/luci-app-wrtbwmon-zh
 
 
 
