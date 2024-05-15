39d437a01054e36cbe9c83340fd49a6ee2ee3ac1
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-15 11:30:22 +0800

    Update diy-part1.sh

--- config/RAX3000M-256M/diy-part1.sh
+++ config/RAX3000M-256M/diy-part1.sh
@@ -10,28 +10,37 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # 添加第三方插件库
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
-https://github.com/sundaqiang/openwrt-packages
 
-#添加额外软件包
-#家长控制
-# git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 
-#魔改版-管控过滤[集成上网时间控制，黑白名单IP过滤，网址过滤几大功能]
-# git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
+# 添加额外软件包
+
+# 定时设置
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+
+# 魔改版-管控过滤[集成上网时间控制，黑白名单IP过滤，网址过滤几大功能]
+git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
+
+# 管控过滤-普通版
+# git clone https://github.com/ywt114/luci-app-control-weburl package/luci-app-control-weburl
+
+# 定时限速
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
 # 在线用户
-# git clone https://github.com/selfcan/luci-app-onliner package/luci-app-onliner
+git clone https://github.com/selfcan/luci-app-onliner package/luci-app-onliner
+# git clone https://github.com/haiibo/luci-app-onliner package/luci-app-onliner
 
+# 应用过滤
+git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 
+# 家长控制
+git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 
+# 网络唤醒++
+git clone https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
 
-
-
-
-
-
-
-
-
+# 实时流量监控
+git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
