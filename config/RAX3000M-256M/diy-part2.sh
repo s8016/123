8ac5f830944b6b2a4ad9127686d86f054bfeebc2
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-16 15:01:39 +0800

    Update diy-part2.sh

--- config/RAX3000M-256M/diy-part2.sh
+++ config/RAX3000M-256M/diy-part2.sh
@@ -38,12 +38,12 @@ rm -rf ./package/istore
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-rm -rf ./feeds/small/luci-app-mosdns
-rm -rf ./feeds/small/mosdns
+# rm -rf ./feeds/small/luci-app-mosdns
+# rm -rf ./feeds/small/mosdns
 
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# rm -rf feeds/packages/net/v2ray-geodata
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golan
