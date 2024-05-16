6ac7dc6cdb6984ff5993f2843ef37bbc4a8e252e
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-16 14:11:55 +0800

    Update diy-part2.sh

--- config/RAX3000M-256M/diy-part2.sh
+++ config/RAX3000M-256M/diy-part2.sh
@@ -20,6 +20,13 @@ sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 sed -i 's/<0x580000 0x7200000>/<0x580000 0xea80000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
 sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
 
+# 修改onliner菜单
+sed -i 's/Online User/在线用户/g' package/luci-app-onliner/po/zh-cn/onliner.po
+sed -i 's/user online/客户端/g' package/luci-app-onliner/po/zh-cn/onliner.po
+sed -i 's/Speed Monitor/网速监控/g' package/luci-app-onliner/po/zh-cn/onliner.po
+sed -i 's/There is no one online now./当前无任何设备在线/g' package/luci-app-onliner/po/zh-cn/onliner.po
+
+
 # 删除冲突的软件包
 rm -rf ./package/istore
 # rm -rf ./feeds/kenzo/luci-app-quickstart
