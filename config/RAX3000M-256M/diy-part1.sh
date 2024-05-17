7ccaceb2ac2614929644575bdf91e9833ab42c10
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-17 10:39:17 +0800

    Update diy-part1.sh

--- config/RAX3000M-256M/diy-part1.sh
+++ config/RAX3000M-256M/diy-part1.sh
@@ -16,10 +16,10 @@ echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 # 添加额外软件包
 
 # 定时设置
-git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+# git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 
 # 魔改版-管控过滤[集成上网时间控制，黑白名单IP过滤，网址过滤几大功能]
-git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
+# git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-control-weburl
 
 # 管控过滤-普通版
 # git clone https://github.com/ywt114/luci-app-control-weburl package/luci-app-control-weburl
@@ -28,7 +28,7 @@ git clone https://github.com/gdck/luci-app-control-weburl package/luci-app-contr
 # git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
 # 在线用户
-git clone https://github.com/ElvenP/luci-app-onliner package/luci-app-onliner
+# git clone https://github.com/ElvenP/luci-app-onliner package/luci-app-onliner
 # git clone https://github.com/selfcan/luci-app-onliner package/luci-app-onliner
 # git clone https://github.com/haiibo/luci-app-onliner package/luci-app-onliner
 
