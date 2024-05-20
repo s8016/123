7362eda3231f86c4685be066d0dce4e110eb0625
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-20 10:47:02 +0800

    Update diy-part2.sh

--- config/123/diy-part2.sh
+++ config/123/diy-part2.sh
@@ -14,11 +14,11 @@
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改 主机名称
-sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+# sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 
 # 修改 闪存为256M
-sed -i 's/<0x580000 0x7200000>/<0x580000 0xea80000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
-sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
+# sed -i 's/<0x580000 0x7200000>/<0x580000 0xea80000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
+# sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
 
 # 更换主题-开始#
 # rm -rf feeds/luci/themes/luci-theme-argon
@@ -48,7 +48,7 @@ sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
 # rm -rf ./package/feeds/smpackage/luci-app-tencentcloud-cos/Makefile
 
 # 删除冲突的软件包
-rm -rf ./package/istore
+# rm -rf ./package/istore
 # rm -rf ./package/feeds/kenzo/luci-app-istorex
 # rm -rf ./feeds/kenzo/luci-app-quickstart
 # rm -rf ./feeds/kenzo/luci-app-store
