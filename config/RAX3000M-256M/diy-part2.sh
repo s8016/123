4c2ecd0b736184e5df5118b5d2970cf02b394632
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-20 13:12:08 +0800

    Update diy-part2.sh

--- config/RAX3000M-256M/diy-part2.sh
+++ config/RAX3000M-256M/diy-part2.sh
@@ -14,7 +14,9 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改 主机名称
-sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+# sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+sed -i 's/MT7981_AX3000/RAX3000M/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i 's/MT7981_AX3000/RAX3000M/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 
 # 修改 闪存为256M
 sed -i 's/<0x580000 0x7200000>/<0x580000 0xea80000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
