1979c2a9fa47c9974cb42ba4e8ce61dc945badd7
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-21 15:23:28 +0800

    Update diy-part1.sh

--- config/123/diy-part1.sh
+++ config/123/diy-part1.sh
@@ -13,6 +13,8 @@
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
 
 
 # 添加额外软件包
