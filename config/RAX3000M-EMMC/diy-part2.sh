c3cde25e52aedc2139f1d9876186ed0d1d4169e2
s8016 <164966593+s8016@users.noreply.github.com>
2024-05-23 11:19:45 +0800

    Update diy-part2.sh

--- config/RAX3000M-EMMC/diy-part2.sh
+++ config/RAX3000M-EMMC/diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # 修改 Lan IP(修改后面的IP）
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改 主机名称
-sed -i 's/ImmortalWrt/RAX-3000Z-加强版/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/RAX3000Z-加强版/g' package/base-files/files/bin/config_generate
 
 #删除冲突的软件包
 #rm -rf ./package/feeds/smpackage/core/Makefile
@@ -25,3 +25,13 @@ sed -i 's/ImmortalWrt/RAX-3000Z-加强版/g' package/base-files/files/bin/config
 #rm -rf ./package/feeds/smpackage/luci-app-homeproxy/Makefile
 #rm -rf ./package/feeds/smpackage/luci-app-macvlan/Makefile
 #rm -rf ./package/feeds/smpackage/luci-app-tencentcloud-cos/Makefile
+
+
+# add mosdns
+rm -rf ./feeds/small/luci-app-mosdns
+rm -rf ./feeds/small/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
